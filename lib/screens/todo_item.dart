9c78409efdd3bef0d8489a2d3ad9f009d543d55a
MayronSouza <mayron.souza@gmz.solutions>
2021-04-26 14:47:30 -0300

    creating the structure of the TodoItem screen

---
+++ lib/screens/todo_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TodoItem extends StatefulWidget {
+  @override
+  _TodoItemState createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tarefa'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+    );
+  }
+}
